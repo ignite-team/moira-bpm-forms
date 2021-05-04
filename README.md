c73e756204fbd75e4588a8b20ab367921e4d08b6
Dario Aldrei <dario.aldrei@flowteam.ozona.es>
2021-05-04 09:10:43 +0200

    html

---
+++ README.md
@@ -0,0 +1,1 @@
+# moira-bpm-forms
---
+++ src/main/java/es/ozona/forms/ProcesoContratacionApplication.java
@@ -0,0 +1,10 @@
+package es.ozona.forms;
+
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("proceso-contratacion")
+public class ProcesoContratacionApplication extends ServletProcessApplication {
+
+}
